34681352e07cbdf8e48d549cf18f9d85ac3a0512
ekim22 <ekim2@ggc.edu>
2021-10-05 09:42:30 -0400

    added OnDestroy to PostList and unsubscribe to the postsChangedSub

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {PostService} from "../post.service";
 import {Post} from "../post.model";
 import {Subscription} from "rxjs";
@@ -8,7 +8,7 @@ import {Subscription} from "rxjs";
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
   posts!: Post[]
   private postsChangedSub!: Subscription;
 
@@ -21,6 +21,10 @@ export class PostListComponent implements OnInit {
         this.posts = posts;
       }
     )
+  }
+
+  ngOnDestroy(): void {
+    this.postsChangedSub.unsubscribe();
   }
 
 }
