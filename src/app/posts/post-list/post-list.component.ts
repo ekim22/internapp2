e3324298cec9b9fb7480b7340ecb5f229b9fad12
ekim22 <ekim2@ggc.edu>
2021-10-12 18:52:11 -0400

    changed accordion control to single button; styled to let the control stick on scroll

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {PostService} from "../post.service";
 import {Post} from "../post.model";
 import {Subscription} from "rxjs";
+import {MatAccordion} from "@angular/material/expansion";
 
 @Component({
   selector: 'app-post-list',
@@ -9,7 +10,8 @@ import {Subscription} from "rxjs";
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit, OnDestroy {
-  posts: Post[] = []
+  posts: Post[] = [];
+  listExpandOrCollapse: string = 'Expand';
   private postsChangedSub!: Subscription;
   public isLoading = false;
 
@@ -32,6 +34,15 @@ export class PostListComponent implements OnInit, OnDestroy {
 
   onDelete(postId: string) {
     this.postService.deletePost(postId);
+  }
+
+  onExpandOrCollapse(accordion: MatAccordion) {
+    if (this.listExpandOrCollapse === 'Collapse') {
+      accordion.closeAll();
+    } else if (this.listExpandOrCollapse === 'Expand') {
+      accordion.openAll();
+    }
+    this.listExpandOrCollapse = this.listExpandOrCollapse === 'Collapse' ? 'Expand' : 'Collapse';
   }
 
 }
