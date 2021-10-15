a92c313b4f0f831241089476afd9c7404ff9ff21
ekim22 <ekim2@ggc.edu>
2021-10-15 16:08:50 -0400

    added login form; login in header (+route); header styling
    We're reusing the form from PostCreate but going back to a TD form so some stuff was removed (like formControl)

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  isLoading = true;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.isLoading = false;
+  }
+
+  onSubmit() {
+    console.log("Authenticating...")
   }
 
 }
