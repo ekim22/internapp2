e399af101502ed51f6fe35431ff4a7dad4881003
ekim22 <ekim2@ggc.edu>
2021-10-18 15:55:26 -0400

    modified post route for user to use bcrypt and hash passwords

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,21 +1,30 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 
 const User = require('../models/user');
 
 const router = express.Router();
 
 router.post('/signup', (req, res, next) => {
-  const user = new User({
-    email: req.body.email,
-    password: req.body.password,
-  });
-  user.save().then(
-      (user) => {
-        res.status(201).json({
-          message: 'User created!',
-          user: user,
+  bcrypt.hash(req.body.password, 10). then(
+      (hash) => {
+        const user = new User({
+          email: req.body.email,
+          password: hash,
         });
-      });
+        user.save().then(
+            (result) => {
+              res.status(201).json({
+                message: 'User created!',
+                result: result,
+              });
+            }).catch((err) => {
+          res.status(500).json({
+            error: err,
+          });
+        });
+      },
+  );
 });
 
 module.exports = router;
