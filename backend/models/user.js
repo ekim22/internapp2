2031303eeb93cd45f3cd78929a39691248d5901e
ekim22 <ekim2@ggc.edu>
2021-10-18 15:51:06 -0400

    validates email as unique; created post route for user

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
+  // eslint-disable-next-line max-len
   // unique: true doesn't validate input; It merely allows mongoose to do some optimizations
   email: {type: String, required: true, unique: true},
   password: {type: String, required: true},
 });
+
+// eslint-disable-next-line max-len
+// We pass mongoose-unique-validator here as a plugin to actually make sure the email is unique.
+userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User', userSchema);
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,21 @@
 const express = require('express');
 
+const User = require('../models/user');
+
 const router = express.Router();
 
 router.post('/signup', (req, res, next) => {
-
+  const user = new User({
+    email: req.body.email,
+    password: req.body.password,
+  });
+  user.save().then(
+      (user) => {
+        res.status(201).json({
+          message: 'User created!',
+          user: user,
+        });
+      });
 });
 
 module.exports = router;
