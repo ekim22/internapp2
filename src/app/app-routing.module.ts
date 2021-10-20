8e27ce2c9c138581dff890a2c010578433fbd38f
ekim22 <ekim2@ggc.edu>
2021-10-20 17:13:04 -0400

    added a guard to PostList and set PostCreate/PostEdit routes as its children

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import {PostListComponent} from "./posts/post-list/post-list.component";
 import {PostCreateComponent} from "./posts/post-create/post-create.component";
 import {LoginComponent} from "./auth/login/login.component";
 import {SignupComponent} from "./auth/signup/signup.component";
+import {AuthGuard} from "./auth/auth.guard";
 
 const routes: Routes = [
-  { path: '', component: PostListComponent },
-  { path: 'create', component: PostCreateComponent },
-  { path: 'edit/:postId', component: PostCreateComponent },
+  { path: '', canActivate: [AuthGuard], component: PostListComponent, children: [
+      { path: 'create', component: PostCreateComponent },
+      { path: 'edit/:postId', component: PostCreateComponent },
+    ] },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
 ];
