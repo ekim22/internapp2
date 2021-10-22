e7b1c1c82ada27b8ece48a37a807f2887286046a
ekim22 <ekim2@ggc.edu>
2021-10-22 16:55:55 -0400

    post content piped to linebreak pipe

--- src/app/utils/linebreak.pipe.ts
+++ src/app/utils/linebreak.pipe.ts
@@ -6,7 +6,7 @@ import {Pipe, PipeTransform} from "@angular/core";
 })
 export class LinebreakPipe implements PipeTransform {
   transform(value: any, ...args: any[]): any {
-    return value.replace(/\\n/g, '<br />');
+    return value.replace(/\n/g, '<br>');
   }
 
 }
