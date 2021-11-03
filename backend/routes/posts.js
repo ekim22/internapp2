6518d63084ef102dd0ffdc54f76ea0c284cea0f5
ekim22 <ekim2@ggc.edu>
2021-11-03 15:51:01 -0400

    refactored storage logic into a separate middleware

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,40 +1,16 @@
 const express = require('express');
-const multer = require('multer');
 
 const checkAuth = require('../middleware/check-auth');
+const fileOps = require('../middleware/file');
 const postController = require('../controllers/post');
 
 const router = express.Router();
 
-const MIME_TYPE_MAP = {
-  'image/png': 'png',
-  'image/jpeg': 'jpg',
-  'image/jpg': 'jpg',
-  'image/gif': 'gif',
-};
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error('Invalid mime type!');
-    if (isValid) {
-      error = null;
-    }
-    cb(error, 'backend/images');
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname.toLowerCase().split(' ').join('-');
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + '-' + Date.now() + '.' + ext);
-  },
-});
 
 router.get('', checkAuth, postController.getPosts );
 router.get('/:id', checkAuth, postController.getPost );
-router.post('', checkAuth, multer({storage: storage}).single('image'),
-    postController.createPost );
-router.patch('/:id', checkAuth, multer({storage: storage}).single('image'),
-    postController.updatePost );
+router.post('', checkAuth, fileOps.storeFile, postController.createPost );
+router.patch('/:id', checkAuth, fileOps.storeFile, postController.updatePost );
 router.delete('/:id', checkAuth, postController.deletePost );
 
 module.exports = router;
