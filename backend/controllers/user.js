7fb4e472dc46e0626b5b2d37f34111b6fc20b9f7
ekim22 <ekim2@ggc.edu>
2021-11-03 15:20:10 -0400

    refactored user logic into a user controller

---
+++ backend/controllers/user.js
@@ -0,0 +1,66 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+
+module.exports.createUser = (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(500).json({
+      message: 'Email and password cannot be empty!',
+    });
+  }
+  bcrypt.hash(req.body.password, 10).then(
+      (hash) => {
+        const user = new User({
+          email: req.body.email,
+          password: hash,
+        });
+        user.save().then(
+            (result) => {
+              res.status(201).json({
+                message: 'User created!',
+                result: result,
+              });
+            }).catch((err) => {
+          res.status(500).json({
+            message: 'Failed to create account!',
+          });
+        });
+      },
+  );
+};
+
+module.exports.login = (req, res) => {
+  let returnedUser;
+  User.findOne({email: req.body.email})
+      .then((user) => {
+        if (!user) {
+          return res.status(401).json({
+            message: 'Invalid credentials!',
+          });
+        } else {
+          returnedUser = user;
+        }
+        return bcrypt.compare(req.body.password, user.password);
+      })
+      .then((result) => {
+        if (!result) {
+          return res.status(401).json({
+            message: 'Invalid password!',
+          });
+        }
+        // The expiresIn in the jwt is NOT the same as expiresIn in the res body.
+        const token = jwt.sign({email: returnedUser.email, userId: returnedUser._id},
+            'secret_this_should_be_longer', {expiresIn: '1h'});
+        res.status(200).json({
+          token: token,
+          expiresIn: 3600,
+        });
+      })
+      .catch(() => {
+        res.status(401).json({
+          message: 'Invalid credentials!',
+        });
+      })
+  ;
+};
