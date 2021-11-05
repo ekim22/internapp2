a79a3bf500793b1327a01a4f6fb387b471503966
ekim22 <ekim2@ggc.edu>
2021-11-04 20:06:26 -0400

    refactored the angular material modules into one module

---
+++ src/app/angular-material.module.ts
@@ -0,0 +1,48 @@
+import {NgModule} from "@angular/core";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatMenuModule } from "@angular/material/menu";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
+import {MatDialogModule} from "@angular/material/dialog";
+
+@NgModule({
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatDividerModule,
+    MatProgressBarModule,
+    MatDialogModule,
+  ],
+  exports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatDividerModule,
+    MatProgressBarModule,
+    MatDialogModule,
+  ]
+})
+export class AngularMaterialModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,27 +7,16 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
 import {ReactiveFormsModule, FormsModule} from "@angular/forms";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatInputModule} from "@angular/material/input";
-import {MatCardModule} from "@angular/material/card";
-import {MatButtonModule} from "@angular/material/button";
 import { HeaderComponent } from './header/header.component';
-import {MatToolbarModule} from "@angular/material/toolbar";
-import { MatIconModule } from "@angular/material/icon";
-import { MatMenuModule } from "@angular/material/menu";
 import { PostListComponent } from './posts/post-list/post-list.component';
-import {MatExpansionModule} from "@angular/material/expansion";
-import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
-import {MatPaginatorModule} from "@angular/material/paginator";
+
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
 import {AuthInterceptor} from "./auth/auth-interceptor";
 import {LinebreakPipe} from "./utils/linebreak.pipe";
-import {MatDividerModule} from "@angular/material/divider";
-import {MatProgressBarModule} from "@angular/material/progress-bar";
 import {ErrorInterceptor} from "./error-interceptor";
-import {MatDialogModule} from "@angular/material/dialog";
 import { ErrorComponent } from './error/error.component';
+import {AngularMaterialModule} from "./angular-material.module";
 
 
 @NgModule({
@@ -47,20 +36,8 @@ import { ErrorComponent } from './error/error.component';
     ReactiveFormsModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatMenuModule,
-    MatExpansionModule,
     HttpClientModule,
-    MatProgressSpinnerModule,
-    MatPaginatorModule,
-    MatDividerModule,
-    MatProgressBarModule,
-    MatDialogModule,
+    AngularMaterialModule,
   ],
   // multi: true simply means there can be multi interceptors so don't override any
   // existing interceptors
