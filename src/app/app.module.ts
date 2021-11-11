d005ca7def4aae6bf323ac0cc3fbe4203e08d3e0
ekim22 <ekim2@ggc.edu>
2021-11-10 20:04:32 -0500

    refactored auth components into auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,37 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {ReactiveFormsModule, FormsModule} from "@angular/forms";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { LoginComponent } from './auth/login/login.component';
 import {AuthInterceptor} from "./auth/auth-interceptor";
 import {LinebreakPipe} from "./utils/linebreak.pipe";
 import {ErrorInterceptor} from "./error-interceptor";
 import { ErrorComponent } from './error/error.component';
 import {AngularMaterialModule} from "./angular-material.module";
 import {PostsModule} from "./posts/posts.module";
+import {AuthModule} from "./auth/auth.module";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SignupComponent,
-    LoginComponent,
     ErrorComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
-    FormsModule,
     BrowserAnimationsModule,
     HttpClientModule,
     AngularMaterialModule,
     PostsModule,
+    AuthModule,
   ],
   // multi: true simply means there can be multi interceptors so don't override any
   // existing interceptors
