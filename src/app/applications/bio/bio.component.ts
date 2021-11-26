3c7bb4a79f98e281981369c15744a379c9424995
ekim22 <ekim2@ggc.edu>
2021-11-26 14:35:18 -0500

    added parts of old bio internship application

--- src/app/applications/bio/bio.component.ts
+++ src/app/applications/bio/bio.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ApplicationsService} from "../applications.service";
 
 @Component({
   selector: 'app-bio',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bio.component.css']
 })
 export class BioComponent implements OnInit {
+  isLinear = false;
+  studentAcademicInfo!: FormGroup;
+  emergencyContactInfo!: FormGroup;
+  mentorInfo!: FormGroup;
+  internshipInfo!: FormGroup;
+  educationalObjectives!: FormGroup;
+  signature!: FormGroup;
+  years! : {
+    label: string,
+    value: string
+  }[]
 
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder,
+              private applicationsService: ApplicationsService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.studentAcademicInfo = this._formBuilder.group({
+      firstCtrl: ['', Validators.required],
+    });
+    this.emergencyContactInfo = this._formBuilder.group({
+      secondCtrl: ['', Validators.required],
+    });
+    this.mentorInfo = this._formBuilder.group({
+      thirdCtrl: ['', Validators.required],
+    });
+    this.internshipInfo = this._formBuilder.group({
+      fourthCtrl: ['', Validators.required],
+    });
+    this.educationalObjectives = this._formBuilder.group({
+      fifthCtrl: ['', Validators.required],
+    });
+    this.signature = this._formBuilder.group({
+      sixthCtrl: ['', Validators.required],
+    });
+    this.years = this.applicationsService.years
   }
-
 }
