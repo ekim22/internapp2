938d8c940fc8414d411bdc0629d3fee7e69c0dec
ekim22 <ekim2@ggc.edu>
2022-01-04 16:29:49 -0500

    added appStatus and appProgress funcs

--- backend/controllers/student.js
+++ backend/controllers/student.js
@@ -71,3 +71,80 @@ module.exports.setAppType = (req, res) => {
       });
 };
 
+module.exports.getAppStatus = (req, res) => {
+  Student.findOne({userId: req.userData.userId})
+      .then((student) => {
+        res.status(201).json({
+          appStatus: student.appStatus,
+        });
+      })
+      .catch(() => {
+        res.status(401).json({
+          message: 'Could not get app status!',
+        });
+      }); ;
+};
+
+module.exports.setAppStatus = (req, res) => {
+  User.findOne({userId: req.userData.userId})
+      .then((user) => {
+        const student = new Student(user);
+        student.appStatus = req.body.appStatus;
+        student.save()
+            .then(
+                () => {
+                  res.status(201).json({
+                    message: 'Application status updated!',
+                  });
+                },
+            )
+            .catch(
+                () => {
+                  res.status(500).json({
+                    message: 'Failed to update application status!',
+                  });
+                },
+            );
+      })
+      .catch(() => {
+        res.status(500).json({
+          message: 'Failed to set application status!',
+        });
+      });
+};
+
+module.exports.getAppProgress = (req, res) => {
+  Student.findOne({_id: req.userData.userId})
+      .then((student) => {
+        res.status(201).json({
+          appProgress: student.appProgress,
+        });
+      })
+      .catch(() => {
+        res.status(401).json({
+          message: 'Could not get app progress!',
+        });
+      });
+};
+
+module.exports.setAppProgress = (req, res) => {
+  Student.findOne({_id: req.userData.userId}, (err, foundDoc) => {
+    console.log(foundDoc);
+    console.log(req.body.appProgress);
+    foundDoc.appProgress = req.body.appProgress;
+    foundDoc.markModified('appProgress');
+    foundDoc.save().then(
+        (result) => {
+          console.log('result' + result);
+          if (result) {
+            res.status(200).json({
+              message: 'App progress updated!',
+            });
+          } else {
+            res.status(401).json({message: 'There was an error updating app progress!'});
+          }
+        },
+    );
+  });
+};
+
