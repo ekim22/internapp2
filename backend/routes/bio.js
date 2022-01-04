28cb3acf2a911b91d7bc280e7c4a00b216781b39
ekim22 <ekim2@ggc.edu>
2022-01-04 16:32:01 -0500

    added routes to bio controller funcs

--- backend/routes/bio.js
+++ backend/routes/bio.js
@@ -1,14 +1,12 @@
 const express = require('express');
-const multer = require('multer');
-const upload = multer();
 
 const checkAuth = require('../middleware/check-auth');
 const bioController = require('../controllers/bio');
 
 const router = express.Router();
 
-
-router.post('', checkAuth, upload.none(), bioController.saveApp );
+router.get('', checkAuth, bioController.getApp);
+router.post('/save', checkAuth, bioController.saveApp);
 
 
 module.exports = router;
