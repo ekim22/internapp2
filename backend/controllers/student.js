bc5612fc76bfd7449a17b0b17e61d8c954cbe55b
ekim22 <ekim2@ggc.edu>
2022-01-04 16:28:49 -0500

    added createStudent func and renamed setProgram to setAppType

--- backend/controllers/student.js
+++ backend/controllers/student.js
@@ -1,16 +1,49 @@
 const User = require('../models/user');
 const Student = require('../models/student');
+const bcrypt = require('bcrypt');
 
-module.exports.getProgram = (req, res) => {
+module.exports.createStudent = (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(500).json({
+      message: 'Email and password cannot be empty!',
+    });
+  }
+  bcrypt.hash(req.body.password, 10).then(
+      (hash) => {
+        const student = new Student({
+          email: req.body.email,
+          password: hash,
+        });
+        student.save().then(
+            (result) => {
+              res.status(201).json({
+                message: 'User created!',
+                result: result,
+              });
+            }).catch((err) => {
+          res.status(500).json({
+            message: 'Failed to create account!',
+          });
+        });
+      },
+  );
+};
+
+module.exports.getAppType = (req, res) => {
   Student.findOne({userId: req.userData.userId})
       .then((student) => {
         res.status(201).json({
           appType: student.appType,
         });
+      })
+      .catch(() => {
+        res.status(401).json({
+          message: 'Could not get app type!',
+        });
       });
 };
 
-module.exports.setProgram = (req, res) => {
+module.exports.setAppType = (req, res) => {
   User.findOne({userId: req.userData.userId})
       .then((user) => {
         const student = new Student(user);
