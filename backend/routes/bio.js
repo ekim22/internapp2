01648ac967551c89bf0a0cdaee5ab7b6528725b1
ekim22 <ekim2@ggc.edu>
2022-01-17 21:45:06 -0500

    added addDocs route that handles bio docs

--- backend/routes/bio.js
+++ backend/routes/bio.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 const checkAuth = require('../middleware/check-auth');
+const fileOps = require('../middleware/file');
 const bioController = require('../controllers/bio');
 
 const router = express.Router();
 
 router.get('', checkAuth, bioController.getApp);
 router.post('/save', checkAuth, bioController.saveApp);
+router.post('/doc/upload', checkAuth, fileOps.storeDocument, bioController.addDocs);
 
 
 module.exports = router;
