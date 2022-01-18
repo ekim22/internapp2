b413f65f6df211a0124701c02a1364637b6cc453
ekim22 <ekim2@ggc.edu>
2022-01-17 21:44:46 -0500

    rewrote multer to handle images and documents separately

--- backend/middleware/file.js
+++ backend/middleware/file.js
@@ -7,7 +7,7 @@ const MIME_TYPE_MAP = {
   'image/gif': 'gif',
 };
 
-const storage = multer.diskStorage({
+const imgStorage = multer.diskStorage({
   destination: (req, file, cb) => {
     const isValid = MIME_TYPE_MAP[file.mimetype];
     let error = new Error('Invalid mime type!');
@@ -23,4 +23,18 @@ const storage = multer.diskStorage({
   },
 });
 
-module.exports.storeFile = multer({storage: storage}).single('image');
+const docStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'backend/docs');
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(' ').join('-');
+    const fileType = req.body.fileType;
+    cb(null, name + '-' + Date.now() + '.' + fileType);
+  },
+});
+
+module.exports.storeImage = multer({storage: imgStorage}).single('image');
+// TODO write post about how the name here only needs to match
+//  with the name field in the FormData object associated with the file
+module.exports.storeDocument = multer({storage: docStorage}).single('document');
